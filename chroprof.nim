226d20008b562ca70c8d83f51134879e335d4eaa
gmega <giuliano.mega@gmail.com>
2024-03-01 09:10:30 -0300

    chronprof -> chroprof

--- chroprof.nim
+++ chroprof.nim
@@ -1,5 +1,5 @@
 when not defined(chronosProfiling):
-  {.error: "chronprof requires -d:chronosProfiling to be enabled".}
+  {.error: "chroprof requires -d:chronosProfiling to be enabled".}
 
 import ./chroprof/api
 
